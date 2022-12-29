b199727c96d0cbe2e60cdfe78ea169f6efe3d038
Sergey <sergeileonov@inbox.ru>
2022-12-29 16:27:58 +0000

    Array base

--- Control test/Program.cs
+++ Control test/Program.cs
@@ -1,4 +1,24 @@
 ﻿// написать программу, которая из имеющегося массива строк 
 //создает новый массив из строк,
 // длина которых меньше либо равна 3 символа
+void Print(string[] arr)
+{
+    int size = arr.Length;
 
+    for (int i = 0; i < size; i++)
+    {   
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] Massiv(int size)
+{
+    string[] arr = new string[size];
+    Console.WriteLine("Enter the array elements: ");
+
+    for (int i = 0; i < size; i++)
+    {  
+        arr[i] = Console.ReadLine();    
+    }
+    return arr;
